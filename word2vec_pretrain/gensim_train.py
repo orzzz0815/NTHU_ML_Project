062ce02bcc05dcbf322ee0a3d0067b4bea7994ef
ZheTing <orzzz0815@gmail.com>
2018-01-20 02:45:11 +0800

    init

---
+++ word2vec_pretrain/gensim_train.py
@@ -0,0 +1,15 @@
+from gensim.models import word2vec
+import logging
+
+def main():
+
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    sentences = word2vec.Text8Corpus("wiki_texts.txt")
+    model = word2vec.Word2Vec(sentences, size=250, sg=1)
+
+    model.save("med250.model.bin")
+
+
+if __name__ == "__main__":
+    main()
+
